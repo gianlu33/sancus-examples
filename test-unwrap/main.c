19c68f9942f1ac37b273a75127f07d8df7ce3b3f
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-10-06 11:28:38 +0200

    fix security len

--- test-unwrap/main.c
+++ test-unwrap/main.c
@@ -7,18 +7,17 @@
 
 #define AD_SIZE 4
 #define MSG_SIZE 4
-#define CIPHER_LEN SANCUS_SECURITY + MSG_SIZE
 
 int main()
 {
     msp430_io_init();
-    printf("Hello!\n");
+    printf("Hello! %d\n", SANCUS_TAG_SIZE);
 
     uint8_t key[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
     uint8_t ad[] = {0,0,0,0};
     uint8_t data[] = {'c', 'i', 'a', 'o'};
     uint8_t cipher[MSG_SIZE];
-    uint8_t tag[SANCUS_SECURITY];
+    uint8_t tag[SANCUS_TAG_SIZE];
     uint8_t data_decrypted[MSG_SIZE];
     int i;
 
