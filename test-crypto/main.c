431ca27d6f50ebfb5d778a4272e7eb893318d39f
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-10-20 15:52:22 +0200

    update test-crypto

--- test-crypto/main.c
+++ test-crypto/main.c
@@ -11,27 +11,36 @@
 
 DECLARE_SM(foo, 0x1234);
 
-// if you uncomment the SM_INPUT macro, check the correct ID with readelf
-#define ENTRY_ID 0
-//SM_INPUT(foo, whatever, data, len) {}
+#define FOO_ENTER_ID 3
+#define ATTEST_ID 1
 
-void SM_ENTRY(foo) foo_enter() {
+#define ENTRY_ID FOO_ENTER_ID
+
+SM_INPUT(foo, whatever, data, len) {}
+
+void SM_ENTRY(foo) foo_enter(uint8_t *challenge, uint16_t len, uint8_t *res) {
     pr_info("Hello from Foo");
+    sancus_tag(challenge, len, res);
+    pr_info("Computed tag");
 }
-
 
 int main() {
     msp430_io_init();
+
+    pr_info("hello!");
 
     int id = sancus_enable(&foo);
     ASSERT(id);
     ASSERT(sm_register_existing(&foo));
 
-    // this always works
-    foo_enter();
+    uint8_t challenge[16], result[16];
 
-    // this does not work with my sancus-compiler and with the SM_INPUT uncommented
-    ASSERT( sm_call_id(id, ENTRY_ID, NULL, 0, NULL) );
+    uint16_t args[] = {(uint16_t) challenge, 16, (uint16_t) result};
+    uint16_t retval;
+
+    // this hangs when executing handle_attest, but succeeds with foo_enter
+    ASSERT( sm_call_id(id, ENTRY_ID, args, 3, &retval) );
+    ASSERT(!retval);
 
     pr_info("All done!\n");
     EXIT();
